4457a25cfa9079d893c67a130197b4121251673e
supriyoju <53818694+supriyoju@users.noreply.github.com>
2019-08-06 15:51:27 +0530

    Create even odd.py

---
+++ even odd.py
@@ -0,0 +1,11 @@
+a=int(inbput('insert a number'))
+b=int(inbput('insert a number'))
+if a>b:
+  c=a
+  a=b
+  b=c
+es='even nos: '
+os='odd nos: '
+for i in range(a,b+1):
+  if i%2==0:
+    es=es+str
